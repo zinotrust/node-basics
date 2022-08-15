b43f4cc0a30aa6a730d1e309513a6934b7540fbe
ZinoTrust <zinotrust@gmail.com>
2022-08-16 00:41:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const { person, people } = require("./person");
+const os = require("os");
+const path = require("path");
+
+// console.log(path);
+// console.log(os.platform());
+
+// CommonJS
+// ES module
+
+// -------------------------
+// const greet = (name) => {
+//   console.log(name);
+// };
+
+// greet("Zino");
+// greet("John Doe");
+
+// console.log(global);
+
+// globalThis.setInterval(() => {
+//   console.log("Mern Stack Course");
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
---
+++ person.js
@@ -0,0 +1,9 @@
+const person = "Zino";
+const people = ["Zino", "John", "Kome"];
+
+// module.exports = person;
+
+module.exports = {
+  person,
+  people,
+};
